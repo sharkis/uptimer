17d61810674ca5d1099c784184ad10d0198f87da
Albert Sharkis <sharkis@sharkis.org>
2022-11-13 20:29:53 -0700

    mvp probably

--- components/UpTimer/UpTimer.tsx
+++ components/UpTimer/UpTimer.tsx
@@ -1,22 +1,44 @@
 import { ReactElement, useState, useEffect } from "react";
 import { View, Text, Button } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const UpTimer = function UpTimer(): ReactElement {
   const [datetime, setDateTime] = useState(null);
-  const [dummy,setDummy] = useState(null);
+  const [dummy, setDummy] = useState(null);
+
+  const saveTime = (time?: number) => {
+    AsyncStorage.setItem("@starttime", time);
+    setDateTime(time);
+  };
+
+  const clearTime = () => {
+    AsyncStorage.removeItem("@starttime");
+    setDateTime(null);
+  };
+
+  if (datetime == null) {
+    const storedDateTime = AsyncStorage.getItem("@starttime")
+      .then((val) => {
+        val && saveTime(val);
+      })
+      .catch((e) => console.log(e));
+  }
 
   useEffect(() => {
     const interval = setInterval(() => setDummy(Date.now()), 1000);
     return () => {
       clearInterval(interval);
     };
-  },[]);
+  }, []);
+
+  const elapsed = Date.now() - datetime;
 
   return (
     <View>
       <Text>Timer here</Text>
-      {datetime && <Text>{datetime-Date.now()}</Text>}
-      <Button title="Start" onPress={() => setDateTime(Date.now())} />
+      {datetime && <Text>{elapsed / 1000}</Text>}
+      <Button title="Start" onPress={() => saveTime(Date.now())} />
+      <Button title="Clear" onPress={clearTime} />
     </View>
   );
 };
